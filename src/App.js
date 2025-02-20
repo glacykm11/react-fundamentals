4fc4a01e3028e937a96a607010c38b6f16834b6c
glacykm11 <glacy.kelly3@gmail.com>
2025-02-20 17:04:36 -0300

    implementing useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 import Header from './Header';
 
-const posts = [
-    { title:  'Title#01', subtitle: 'Sub#01', likes: 20 },
-    { title:  'Title#02', subtitle: 'Sub#02', likes: 10 },
-    { title:  'Title#03', subtitle: 'Sub#03', likes: 50 },
-    { title:  'Title#04', subtitle: 'Sub#04', likes: 60 },
-]
-
 function App(){
+    const [posts, setPosts] = useState([
+        { id: Math.random(), title:  'Title#01', subtitle: 'Sub#01', likes: 20 },
+        { id: Math.random(), title:  'Title#02', subtitle: 'Sub#02', likes: 10 },
+        { id: Math.random(), title:  'Title#03', subtitle: 'Sub#03', likes: 50 },
+        { id: Math.random(), title:  'Title#04', subtitle: 'Sub#04', likes: 60 },
+    ]);
+    
+    function handleRefresh(){
+        setPosts((prevState) => [
+            ...prevState, 
+            { 
+                id: Math.random(), 
+                title:  `Title#0${prevState.length+1}`, 
+                subtitle: `Sub#0${prevState.length+1}`, 
+                likes: 60 
+            }
+        ]);
+    }
+
     return (
         <>
             <Header>
-                <h2>Posts da semana</h2>
+                <h2>
+                    Posts da semana
+                    <button onClick={handleRefresh}>Atualizar</button>
+                </h2>
             </Header>
             <hr />
 
