a19890e1d8cacf48d66bf7ee742ed5b29e8156e5
glacykm11 <glacy.kelly3@gmail.com>
2025-02-20 11:15:39 -0300

    rendering lists

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import React from 'react';
 import Post from './Post';
 import Header from './Header';
 
+const posts = [
+    { title:  'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { title:  'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { title:  'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { title:  'Title#04', subtitle: 'Sub#04', likes: 60 },
+]
+
 function App(){
     return (
         <>
@@ -11,33 +18,16 @@ function App(){
             </Header>
             <hr />
 
-            <Post 
-                likes={20}
-                post={{
-                    title: "Título da notícia 01",
-                    subtitle: "Subtítulo da notícia 01"
-                }}
-            />
-
-            <Post 
-                likes={10}
-                post={{
-                    title: "Título da notícia 02",
-                    subtitle: "Subtítulo da notícia 02"
-                }}
-            />
-            <Post 
-                post={{
-                    title: "Título da notícia 03",
-                    subtitle: "Subtítulo da notícia 03"
-                }}
-            />
-            <Post 
-                post={{
-                    title: "Título da notícia 04",
-                    subtitle: "Subtítulo da notícia 04"
-                }}
-            />
+            {posts.map(post => (
+                <Post 
+                    key={post.title}
+                    post={{
+                        title: post.title,
+                        subtitle: post.subtitle
+                    }} 
+                    likes={post.likes}
+                />
+            ))}
         </>
     )
 }
